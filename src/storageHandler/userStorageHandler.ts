022cc8bb749564cf8985a937aed7ce592e59b13c
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-08-06 12:12:33 +0600

    add user local storage handlers

--- src/storageHandler/userStorageHandler.ts
+++ src/storageHandler/userStorageHandler.ts
@@ -1,3 +1,21 @@
-export const getUserFromLocalStorage = (email: string) => {
+import { User } from "../types/interfaces/user.interface"
 
+export const getUsersFromLocalStorage = (): User[] => {
+  const users = JSON.parse(localStorage.getItem('users') || '[]')
+  return users
+}
+
+export const getUserFromLocalStorage = (email: string) => {
+  const users = getUsersFromLocalStorage()
+  return users.find((u: User) => u.email === email)
+}
+
+export const createUserInLocalStorage = (user: User) => {
+  const existUser = getUserFromLocalStorage(user.email)
+  if (existUser) {
+    return existUser
+  }
+  const users = getUsersFromLocalStorage()
+  users.push(user)
+  localStorage.setItem('users', JSON.stringify(users))
 }
