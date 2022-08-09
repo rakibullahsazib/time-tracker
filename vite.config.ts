74e340dfb07e14c9565e82c562db18e20f2fd58d
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-08-09 19:29:57 +0600

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/time-tracker/',
   test: {
     globals: true,
     environment: 'jsdom',
